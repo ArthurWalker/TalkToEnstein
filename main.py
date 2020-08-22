8c24f76177fcc5152454a34197177ba7ae4597ae
ArthurWalker <phuc.phamhuu2@mail.dcu.ie>
2020-08-22 02:42:58 +0100

    Capture faces

--- main.py
+++ main.py
@@ -3,25 +3,30 @@ import sys
 import numpy
 import pandas
 
+def capture_face_picture():
+    image = cv2.imread("beautiful_girl.jpg")
+    faceCascde = cv2.CascadeClassifier(cv2.data.haarcascades+"\haarcascade_frontalface_default.xml")
+    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    faces = faceCascde.detectMultiScale(gray, scaleFactor = 1.2, minNeighbors=3, minSize=(30,30), flags = cv2.CASCADE_SCALE_IMAGE)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    status = cv2.imwrite('faces_detected.jpg', image)
+    print("[INFO] Image faces_detected.jpg written to filesystem: ", status)
 
-def main():
+def capture_face_frame():
     #loads the face cascade into memory so it’s ready for use
-    faceCascde = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    faceCascde = cv2.CascadeClassifier(cv2.data.haarcascades+"\haarcascade_frontalface_default.xml")
     video_capture = cv2.VideoCapture(0)
     while True:
         # Cature 1 frame
         ret, frame = video_capture.read()
-
         # Change the frame to gray color
         gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-
         # Capture the face
         faces = faceCascde.detectMultiScale(gray, scaleFactor = 1.2, minNeighbors=5, minSize=(30,30), flags = cv2.CASCADE_SCALE_IMAGE)
-
         # Draw a rectangle around the faces:
         for (x,y,w,h) in faces:
             cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
-
         # Display the result
         cv2.imshow('Video',frame)
 
@@ -33,4 +38,5 @@ def main():
     cv2.destroyAllWindows()
 
 if __name__=='__main__':
-    main()
+    capture_face_picture()
+    capture_face_frame()
